ae4559cc18f1ab74e643fad193ad8aa835a1f032
Muhammad Jarir Kanji <mjkanji@outlook.com>
2024-02-22 08:59:17 +0500

    Initial commit of dagster-skypilot project.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dagster_skypilot",
+    packages=find_packages(exclude=["dagster_skypilot_tests"]),
+    install_requires=[
+        "dagster>=1.6.0,<1.7.0",
+        "dagster-cloud",
+        "skypilot[aws,azure,gcp]",
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
